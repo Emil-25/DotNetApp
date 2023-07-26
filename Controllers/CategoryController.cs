2f941a55876687ebac590051a26cdf303b09b3d6
emil-25 <emilrahimov25@gmail.com>
2023-07-27 00:18:14 +0400

    Add Part of CRUD operations

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using DotNetApp.Data;
+using DotNetApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj) 
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DotNetApp.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Display Order")]
+        public string DisplayOrder { get; set; } 
+    }
+}
